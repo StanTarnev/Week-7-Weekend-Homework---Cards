759effcc3bc7cb987f59a726f03e88f47a9a314d
Stan Tarnev <stan.tarnev@gmail.com>
2021-02-14 00:57:45 +0000

    working 'showrare' button

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,17 @@
 <template lang="html">
-  <div id="app">
-    <div id="header">
-      <img id="mtg-logo" src="./assets/mtg-logo.jpg" alt="Magic: The Gathering Logo">
-      <h1 id="title">CARDS, 10th Edition</h1>
+    <div id="app">
+        <div id="header">
+            <img id="mtg-logo" src="./assets/mtg-logo.jpg" alt="Magic: The Gathering Logo">
+            <h1 id="title">CARDS, 10th Edition</h1>
+        </div>
+        <h1 v-if="!cards.length">LOADING...</h1>
+        <button v-on:click="show('Rare')">Show Rare</button>
+        <h3 id="click-instruction">Click on a card to see details</h3>
+        <div id="list-info" v-if="cards">
+            <card-list :cards="cards" />
+            <card-detail v-if="selectedCard" :card="selectedCard" />
+        </div>
     </div>
-    <h1 v-if="!cards.length">LOADING...</h1>
-    <h3 id="click-instruction">Click on a card to see details</h3>
-    <div id="list-info" v-if="cards">
-      <card-list :cards="cards" />
-      <card-detail v-if="selectedCard" :card="selectedCard" />
-    </div>
-  </div>
 </template>
 
 <script>
@@ -20,72 +21,75 @@ import CardDetail from  './components/CardDetail.vue';
 import CardList from './components/CardList';
 
 export default {
-  name: 'app',
-  data(){
-    return{
-      cards: [],
-      selectedCard: null,
-    }
-  },
-  methods: {
-    removeDuplicates(cards){
-      let uniqueCards = [];
-      for (let myCard of cards.cards) {
-        if (myCard.hasOwnProperty("imageUrl") && myCard.set === "10E") {
-          uniqueCards.push(myCard);
+    name: 'app',
+    data(){
+        return{
+            cards: [],
+            selectedCard: null,
         }
-      }
+    },
+    methods: {
+        removeDuplicates(cards){
+            let uniqueCards = [];
+            for (let myCard of cards.cards) {
+                if (myCard.hasOwnProperty("imageUrl") && myCard.set === "10E") {
+                    uniqueCards.push(myCard);
+                }
+            }
 
-      return uniqueCards;
+            return uniqueCards;
+        },
+        show(property) {
+            this.cards = this.cards.filter(card => card.rarity == property)
+        }
+    },
+    mounted(){
+        fetch('http://localhost:3000/cards-data')
+        .then(result => result.json())
+        .then((cards) => {
+            this.cards = this.removeDuplicates(cards);
+        })
+
+        eventBus.$on('card-selected', (card) => {
+            this.selectedCard = card;
+        })
+    },
+    components: {
+        'card-detail': CardDetail,
+        'card-list': CardList
     }
-  },
-  mounted(){
-    fetch('http://localhost:3000/cards-data')
-    .then(result => result.json())
-    .then((cards) => {
-      this.cards = this.removeDuplicates(cards);
-    })
-
-    eventBus.$on('card-selected', (card) => {
-      this.selectedCard = card;
-    })
-  },
-  components: {
-    'card-detail': CardDetail,
-    'card-list': CardList
-  }
 }
 </script>
 
 <style lang="css" >
 body {
-  background-color: black;
-  color: white;
-  font-family: 'papyrus';
+    background-color: black;
+    color: white;
+    font-family: 'papyrus';
 }
 
 #list-info {
-  display: flex;
+    display: flex;
 }
 
 #header {
-  display: flex;
-  justify-content: space-around;
-  width: 85%;
+    display: flex;
+    justify-content: space-around;
+    width: 85%;
 }
 
 #mtg-logo {
-  height: 5em;
+    height: 5em;
 }
 
 #click-instruction {
-  padding-left: 13%;
-  margin-bottom: 3px;
-  margin-top: 10px;
+    padding-left: 13%;
+    margin-bottom: 3px;
+    margin-top: 10px;
 }
 
 #title {
-  color: orange;
-  font-weight: bold;
+    color: orange;
+    font-weight: bold;
 }
 </style>
