d6ff127137329fc0a89cb39f2376e66e5a66a4cc
Stan Tarnev <stan.tarnev@gmail.com>
2021-02-26 02:06:07 +0000

    Moved artist info to the top of the details

--- client/src/components/CardDetail.vue
+++ client/src/components/CardDetail.vue
@@ -5,12 +5,12 @@
     </div>
     <div id="details">
       <h2>{{card.name}}</h2>
+      <p><span>Artist:  </span>{{card.artist}}</p>
       <h4><span>Set:  </span>{{card.setName}}</h4>
       <p>{{card.flavor}}</p>
       <h4><span>Type:  </span>{{card.type}}</h4>
       <h4><span>Rarity:  </span>{{card.rarity}}</h4>
       <p>{{card.text}}</p>
-      <p><span>Artist:  </span>{{card.artist}}</p>
     </div>
   </div>
 </template>
